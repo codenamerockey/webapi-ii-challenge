6109455aaa49d0f53001747ed9f55512f9cafc08
Rockey Simmons <codenamerockey@gmail.com>
2019-10-17 21:43:41 -0700

    set up for postgres through heroku

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const port = process.env.PORT || 8080;
 server.use(express.json());
 server.use('/api/posts', postRoutes);
 
-// server.get('/', (req, res) => {
-//   res.send(`<h1>Hello from server 8080</h1>`);
-// });
+server.get('/', (req, res) => {
+  res.send(
+    `<h3>https://webapi2postgres.herokuapp.com</h3>, https://webapi2postgres.herokuapp.com/api/posts gives you all posts</h3>`
+  );
+});
 
 server.listen(port, () => console.log(`\n** server running on port ${port}`));
