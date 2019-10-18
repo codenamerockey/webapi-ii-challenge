097ec0d7d372756c3e40a33fcc734ae537f2b70f
Rockey Simmons <codenamerockey@gmail.com>
2019-10-17 20:57:29 -0700

    updated my index.js file

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const port = process.env.PORT || 8080;
 server.use(express.json());
 server.use('/api/posts', postRoutes);
 
-server.get('/', (req, res) => {
-  res.send(`<h1>Hello from server 8080</h1>`);
-});
+// server.get('/', (req, res) => {
+//   res.send(`<h1>Hello from server 8080</h1>`);
+// });
 
 server.listen(port, () => console.log(`\n** server running on port ${port}`));
